7f7cdbb7805f8eec7ca1a923b37b4a8716aa2fe6
seung567 <82252998+seung567@users.noreply.github.com>
2024-04-30 21:37:03 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			int numA = sc.nextInt();
+			int numB = sc.nextInt();
+			System.out.println(numA+numB);
+			break;
+		}
+	}
+}
